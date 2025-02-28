1b464a6794b577e5ebe285dda0d172c936d9e92d
zdebar <157598603+zdebar@users.noreply.github.com>
2025-02-28 10:26:44 +0100

    opravt Shopping Cart

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { CartProvider } from './context/CartContext';
+import { CartProvider } from "./context/CartProvider.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
